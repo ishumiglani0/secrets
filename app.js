7a8760a2de89164f48df82f1c91e6ad03086ef34
Ishu <88482780+ishumiglani0@users.noreply.github.com>
2023-07-21 07:47:10 +0530

    Added Styling

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
-const passportLocalMongoose = require('passport-local-mongoose')
+const passportLocalMongoose = require("passport-local-mongoose");
 const { Strategy: GoogleStrategy } = require("passport-google-oauth20");
 const findOrCreate = require("mongoose-findorcreate");
-const favicon = require('serve-favicon');
+const favicon = require("serve-favicon");
 
 const app = express();
-app.use(favicon(__dirname + '/favicon.ico'));
-app.use(express.static(__dirname+"/public/"));
+app.use(favicon(__dirname + "/favicon.ico"));
+app.use(express.static(__dirname + "/public/"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
@@ -22,7 +22,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 1800000, // 1/2 hour in milliseconds
+      maxAge: 1800000, // 1/2 hour in milliseconds
     },
   })
 );
@@ -31,10 +31,13 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 mongoose
-  .connect(`mongodb+srv://${process.env.ADMIN}:${process.env.PASSWORD}@cluster0.abvqgur.mongodb.net/userDB`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    `mongodb+srv://${process.env.ADMIN}:${process.env.PASSWORD}@cluster0.abvqgur.mongodb.net/userDB`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     console.log("Connected to the database Successfully!!!");
   })
@@ -46,7 +49,7 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
   googleId: String,
-  secrets: Array
+  secrets: Array,
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -79,13 +82,15 @@ passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
-    User.findById(id).then((user) =>{
+passport.deserializeUser(function (id, done) {
+  User.findById(id)
+    .then((user) => {
       done(null, user);
-    }).catch((err)=>{
-        done(err)
+    })
+    .catch((err) => {
+      done(err);
     });
-  });
+});
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -100,72 +105,97 @@ app.get(
   "/auth/google/secrets",
   passport.authenticate("google", { failureRedirect: "/login" }),
   (req, res) => {
+    User.collection.dropIndex("username_1");
     res.redirect("/secrets");
   }
 );
 
-app.get("/secrets",(req, res)=>{
-    User.find({"secrets": {$ne: null}}).then((foundUsers)=>{
-        if (foundUsers) {
-          console.log(foundUsers)  
-          foundUsers.map((user)=>{
-            user._id = user._id.toString()
-          })
-          console.log(foundUsers)  
+app.get("/secrets", (req, res) => {
+  User.find({ secrets: { $ne: null } })
+    .then((foundUsers) => {
+      console.log("reached2");
+      if (foundUsers) {
+        console.log(foundUsers);
+        foundUsers.map((user) => {
+          user._id = user._id.toString();
+        });
+        console.log(foundUsers);
 
-            res.render("secrets", {usersWithSecrets: foundUsers});
-          }
-    }).catch((err)=>{
-        if (err){
-            console.log(err);
-          } 
+        res.render("secrets", { usersWithSecrets: foundUsers });
+      }
     })
-});
-app.get("/secrets/:id/:no",(req, res)=>{
-    User.findById(req.params.id).then((foundUser)=>{
-        if (foundUser) {
-          console.log(foundUser)
-            res.render("post", {secret: foundUser.secrets[req.params.no]});
-          }
-    }).catch((err)=>{
-        if (err){
-            console.log(err);
-          } 
-    })
-});
-
-  
-  app.get("/submit", (req, res)=>{
-    if (req.isAuthenticated()){
-      res.render("submit");
-    } else {
-      res.redirect("/login");
-    }
-  });
-  
-  app.post("/submit", function(req, res){
-    console.log('entered')
-    const submittedSecret = req.body.secret;
-    console.log(submittedSecret)
-  
-  //Once the user is authenticated and their session gets saved, their user details are saved to req.user.
-    // console.log(req.user.id);
-  
-    User.findById(req.user.id).then((foundUser)=>{
-        if (foundUser) {
-            console.log(foundUser)
-            foundUser.secrets.push(submittedSecret);
-            foundUser.save().then(()=>{
-              res.redirect("/secrets");
-            });
-          }
-    }).catch((err)=>{
-        if (err) {
-            console.log(err);
-          }
+    .catch((err) => {
+      if (err) {
+        console.log(err);
+      }
     });
 });
 
+app.get("/own", (req, res) => {
+  if (req.isAuthenticated()) {
+    User.findById(req.user.id)
+    .then((foundUser) => {
+      if (foundUser) {
+        res.render("own",{secrets:foundUser.secrets,userid:req.user.id})
+      }
+    })
+    .catch((err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/secrets/:id/:no", (req, res) => {
+  User.findById(req.params.id)
+    .then((foundUser) => {
+      if (foundUser) {
+        console.log(foundUser);
+        res.render("post", { secret: foundUser.secrets[req.params.no] });
+      }
+    })
+    .catch((err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+});
+
+app.get("/submit", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("submit");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.post("/submit", function (req, res) {
+  console.log("entered");
+  const submittedSecret = req.body.secret;
+  console.log(submittedSecret);
+
+  //Once the user is authenticated and their session gets saved, their user details are saved to req.user.
+  console.log( req.user.id);
+
+  User.findById(req.user.id)
+    .then((foundUser) => {
+      if (foundUser) {
+        console.log(foundUser);
+        foundUser.secrets.push(submittedSecret);
+        foundUser.save().then(() => {
+          res.redirect("/secrets");
+        });
+      }
+    })
+    .catch((err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+});
 
 app
   .route("/login")
@@ -201,11 +231,11 @@ app
     );
   });
 
-  app.get("/logout", (req, res) => {
-    req.logout(() => {
-      res.redirect("/");
-    });
+app.get("/logout", (req, res) => {
+  req.logout(() => {
+    res.redirect("/");
   });
+});
 
 const port = process.env.PORT || 3000;
 
